1e08c11d025ce881cd43e8d180e13337fa5b444c
Ghanistkumar Patel <56911755+Ghanistkumar@users.noreply.github.com>
2024-09-26 16:21:14 +0530

    updated football spider

--- football_scraper/spiders/FootballSpider.py
+++ football_scraper/spiders/FootballSpider.py
@@ -13,12 +13,12 @@ class FootballSpider(scrapy.Spider):
         Parse the Wikipedia page to extract league names and URLs.
         """
         leagues = []
-        for row in response.xpath('//*[@id="mw-content-text"]/div[1]/table[3]/tbody/tr')[1:]:  # Skipping header row
-            league_name = row.xpath('//td[3]/p/b/a').get()
-            league_url = row.xpath('//td[3]/p/b/a/@href').get()
+        for row in response.xpath('//*[@class="wikitable"]/tbody/tr/td/p/b/a')[1:]:  # Skipping header row
+            league_name = row.xpath('@title').get()
+            league_url = row.xpath('@href').get()
 
-            # Only proceed if both league_name and league_url are found
-            if league_name and league_url:
+            # Only proceed if both league_name and league_url are found and if the URL belongs to Wikipedia
+            if league_name and league_url and league_url.startswith('/wiki/'):
                 league_name = league_name.strip()
                 full_league_url = response.urljoin(league_url)
                 leagues.append({
@@ -26,7 +26,6 @@ class FootballSpider(scrapy.Spider):
                     'league_url': full_league_url
                 })
                 yield response.follow(full_league_url, self.parse_league, meta={'league_name': league_name})
-
 
         # Save the extracted league data
         with open('leagues.json', 'w') as f:
@@ -39,17 +38,19 @@ class FootballSpider(scrapy.Spider):
         league_name = response.meta['league_name']
         clubs = []
         
-        # Scraping club links
-        for club_link in response.css('a::attr(href)').getall():
-            club_name = response.css('a::text').get()
-            club_url = response.urljoin(club_link)
-            if club_url:
-                clubs.append({
-                    'club_name': club_name,
-                    'club_url': club_url
-                })
-                # Follow each club's link to extract player data
-                yield response.follow(club_url, self.parse_club, meta={'club_name': club_name, 'league_name': league_name})
+        # Scraping club links and ensuring they are Wikipedia links
+        for club_link in response.xpath('//*[@class="wikitable"]/tbody/tr/td/a'):
+            club_name = club_link.xpath('@title').get()
+            if(club_link.xpath('@href').get() is not None):
+                club_url = 'https://en.wikipedia.org' + club_link.xpath('@href').get()
+            else:
+                club_url = ''
+            clubs.append({
+                'club_name': club_name,
+                'club_url': club_url
+            })
+            # Follow each club's link to extract player data
+            yield response.follow(club_url, self.parse_club, meta={'club_name': club_name, 'league_name': league_name})
         
         # Save club data segmented by league
         league_clubs = {
@@ -66,24 +67,42 @@ class FootballSpider(scrapy.Spider):
         club_name = response.meta['club_name']
         league_name = response.meta['league_name']
         players = []
-
+        #list of league done, list of club done, visit each club and get player list and visit each player get player details remaining
         # Scraping player information (this needs to be tailored per website structure)
-        for player in response.css('div.player-info'):
-            player_name = player.css('span.player-name::text').get()
-            player_position = player.css('span.player-position::text').get()
-            player_age = player.css('span.player-age::text').get()
-            player_data = {
-                'name': player_name,
-                'position': player_position,
-                'age': player_age
-            }
-            players.append(player_data)
+        for player in response.xpath('//*[contains(@class,"football-squad")]/tbody/tr/td[4]/span/a'):
+        #     player_name = player.xpath('//tr[th/a/text()="FullName"]/td').get()
+        #     player_position = player.xpath('//tr[th/a/text()="Position(s)"]/td').get()
+        #     player_age = player.xpath('//tr[th/a/text()="Date of birth"]/td').get()
+        #     player_data = {
+        #         'name': player_name,
+        #         'position': player_position,
+        #         'age': player_age
+        #     }
+        #     players.append(player_data)
 
-        # Save player data segmented by league and club
-        club_players = {
-            'league_name': league_name,
-            'club_name': club_name,
-            'players': players
-        }
-        with open('players.json', 'a') as f:
-            json.dump(club_players, f, indent=4)
+        # # Save player data segmented by league and club
+        # club_players = {
+        #     'league_name': league_name,
+        #     'club_name': club_name,
+        #     'players': players
+        # }
+        # with open('players.json', 'a') as f:
+        #     json.dump(club_players, f, indent=4)
+            yield response.follow(player_url, self.parse_club, meta={'club_name': club_name, 'league_name': league_name})
+    
+    def parse_player(self, response):
+        listingUrls = []
+        for item in response.xpath(x):
+            listing_url = item.xpath('.//@href').extract()
+            if listing_url and len(listing_url) > 0:
+                absoluteUrl = getAbsoluteUrl(response.request.url, listing_url[0])
+                yield listingUrls.append(absoluteUrl)
+
+        if len(listingUrls) > 0:
+            for url in listingUrls:
+                yield Request(url=url, callback=self.parse_player_data, meta={'listing_url': url})
+                time.sleep(5)
+    
+    def parse_player_data:
+        pass
+
