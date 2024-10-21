dc7ff95c8dbed6b257f92308c8c7ef6fc778e1ab
Ghanistkumar <ghanistkumar.p@ciright.com>
2024-10-21 17:25:03 +0530

    Improved spider

--- football_scraper/spiders/clubs.py
+++ football_scraper/spiders/clubs.py
@@ -1,4 +1,5 @@
 import scrapy, json, time
+from football_scraper.config.config import locations
 from football_scraper.helpers.scraper_handler import getAbsoluteUrl
 from scrapy.loader import ItemLoader
 from football_scraper.items import ClubsItem
@@ -7,35 +8,26 @@ from scrapy import Request
 class ClubsSpider(scrapy.Spider):
     name = 'clubs'
     
-    # Starting URL (English football club system on Wikipedia)
-    start_urls = ['https://en.wikipedia.org/wiki/English_football_league_system']
+    def start_requests(self):
+        baseUrls = locations.leagues_url
+        for reqUrl in baseUrls:
+            yield Request(url=reqUrl, callback=self.parse, meta={'dont_redirect': True})
+
 
     def parse(self, response):
-        for row in response.xpath('//*[@class="wikitable"]/tbody/tr/td/p//a')[1:]:  # Skipping header row
-            league_name = row.xpath('@title').get()
-            league_url = getAbsoluteUrl(response.request.url, row.xpath('@href').get())
-        yield Request(league_url, self.parseClub, meta={'club_url': league_url})
-        
-
-    def parseClub(self, response):
         listingUrls = []
-        for item in response.xpath('//*[@id="mw-content-text"]/div[1]/table[2]/tbody/tr/td[2]/a'):
-            listing_url = getAbsoluteUrl(response.request.url, item.xpath('@href').get())
-            print('aa',item)
-            if listing_url:
-                listingUrls.append(listing_url)  # Just append to the list, no need to yield here.
-        
-        print(listingUrls)
-        if listingUrls:  # Check if listingUrls is not empty
-            for url in listingUrls:
-                yield Request(url, self.parseClubData, meta={'club_url': url})
-                time.sleep(3)  # Be careful with sleep, it can slow down your crawl significantly
-
-                
-
-        # Save the extracted club data
-        # with open('clubs.json', 'w') as f:
-        #     json.dump(clubs, f, indent=4)
+        table_head_xpath = '//table[@class="wikitable"]/tbody/tr[th/text()="Club\n"]'
+        if(response.xpath(table_head_xpath)) is not None:
+            for item in response.xpath('//table[@class="wikitable"]/tbody/tr/td[1]/a'):
+                listing_url = getAbsoluteUrl(response.request.url, item.xpath('@href').get())
+                if listing_url:
+                    listingUrls.append(listing_url) 
+            
+            # print(listingUrls)
+            if listingUrls:
+                for url in listingUrls:
+                    yield Request(url, self.parseClubData, meta={'club_url': url})
+                    time.sleep(3)
 
 
     def parseClubData(self, response):
@@ -53,13 +45,13 @@ class ClubsSpider(scrapy.Spider):
         # clubItems.add_xpath('insta_profile', './/a[@class="insta-profile"]/@href')
         # clubItems.add_xpath('linkedin_profile', './/a[@class="linkedin-profile"]/@href')
         # clubItems.add_xpath('twitter_profile', './/a[@class="twitter-profile"]/@href')
-        clubItems.add_value('website_url', response.xpath('//ul/li/a[contains(@class, "external text")]').get())
+        clubItems.add_value('website_url', response.xpath('//ul/li/a[contains(@class, "external text")]/@href').get())
         # clubItems.add_xpath('post_code', './/span[@class="post-code"]/text()')
         # clubItems.add_xpath('address', './/span[@class="address"]/text()')
         clubItems.add_xpath('country', '//tr[th/text()="Country"]/td')
         # clubItems.add_xpath('state', './/span[@class="state"]/text()')
         # clubItems.add_xpath('city', './/span[@class="city"]/text()')
         # clubItems.add_xpath('zipcode', './/span[@class="zipcode"]/text()')
-        clubItems.add_value('logo', 'https:' + response.xpath('//img[@class="mw-file-element"][1]').get())
+        clubItems.add_value('logo', 'https:' + response.xpath('//img[@class="mw-file-element"]/@src[1]').get())
 
         yield clubItems.load_item()
